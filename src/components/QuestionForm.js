f014bba086440095f923c23d4f038e1a7d6c3790
Samanyu24X <35706393+Samanyu24X@users.noreply.github.com>
2024-12-23 22:14:24 -0800

    removed hardcoded stuff

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -2,6 +2,63 @@ import React, { useState, useEffect, useMemo } from 'react';
 import questions from './questions';
 import OutputPage from './OutputPage'; // Ensure OutputPage is correctly imported
 
+const stateNameToAbbreviation = {
+  'Alabama': 'AL',
+  'Alaska': 'AK',
+  'Arizona': 'AZ',
+  'Arkansas': 'AR',
+  'California': 'CA',
+  'Colorado': 'CO',
+  'Connecticut': 'CT',
+  'Delaware': 'DE',
+  'Florida': 'FL',
+  'Georgia': 'GA',
+  'Hawaii': 'HI',
+  'Idaho': 'ID',
+  'Illinois': 'IL',
+  'Indiana': 'IN',
+  'Iowa': 'IA',
+  'Kansas': 'KS',
+  'Kentucky': 'KY',
+  'Louisiana': 'LA',
+  'Maine': 'ME',
+  'Maryland': 'MD',
+  'Massachusetts': 'MA',
+  'Michigan': 'MI',
+  'Minnesota': 'MN',
+  'Mississippi': 'MS',
+  'Missouri': 'MO',
+  'Montana': 'MT',
+  'Nebraska': 'NE',
+  'Nevada': 'NV',
+  'New Hampshire': 'NH',
+  'New Jersey': 'NJ',
+  'New Mexico': 'NM',
+  'New York': 'NY',
+  'North Carolina': 'NC',
+  'North Dakota': 'ND',
+  'Ohio': 'OH',
+  'Oklahoma': 'OK',
+  'Oregon': 'OR',
+  'Pennsylvania': 'PA',
+  'Rhode Island': 'RI',
+  'South Carolina': 'SC',
+  'South Dakota': 'SD',
+  'Tennessee': 'TN',
+  'Texas': 'TX',
+  'Utah': 'UT',
+  'Vermont': 'VT',
+  'Virginia': 'VA',
+  'Washington': 'WA',
+  'West Virginia': 'WV',
+  'Wisconsin': 'WI',
+  'Wyoming': 'WY'
+};
+
+const convertStateListToAbbreviations = (stateList) => {
+  return stateList.map(stateName => stateNameToAbbreviation[stateName] || stateName);
+};
+
 const QuestionForm = () => {
   const initialFormData = questions.reduce((acc, q) => {
     if (q.id === 'states') {
@@ -81,8 +138,7 @@ const QuestionForm = () => {
       // gpa: formData.gpa, // This maps to the gpa slider
       satScoreMath: formData.satScoreMath, // This maps to the SAT score slider
       satScoreEnglish: formData.satScoreEnglish,
-      // state: formData.state,
-      state: ["CA", "TX", "FL"],
+      state: convertStateListToAbbreviations(formData.states),
     };
 
     try {
